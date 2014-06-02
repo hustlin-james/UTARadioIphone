2bcf2783c40fcf5bdffc7e959cab0cf40decea56
UTA-Mobi <jamesfielder@Jamess-MacBook-Pro-2.local>
2014-06-02 11:53:09 -0500

    UTA Radio Iphone
    the newest uta radio iPhone app

---
+++ UTARadioIphone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UTARadioIphone
+//
+//  Created by James Fielder on 6/2/14.
+//  Copyright (c) 2014 com.mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UTARadioIphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UTARadioIphone
+//
+//  Created by James Fielder on 6/2/14.
+//  Copyright (c) 2014 com.mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
